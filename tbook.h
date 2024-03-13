0f49e18e4d8b479e0949e96a838e0dec49507481
xdexst <rnoy10303@gmail.com>
2024-03-13 23:29:54 +0400

    Создан заголовочный файл tbook.h с определением класса TBook, представляющего книгу.

---
+++ tbook.h
@@ -0,0 +1,15 @@
+#ifndef TBOOK_H
+#define TBOOK_H
+
+#include <string>
+using namespace std;
+
+class TBook {
+public:
+    string Name;
+    string Author;
+    int Count;
+    TBook();
+};
+
+#endif
